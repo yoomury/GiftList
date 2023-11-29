7556631d6cb0b648eac7977f29737a775c1e3408
Vladut <vladut@argent.xyz>
2023-11-29 11:17:16 +0200

    Add solution

--- server/index.js
+++ server/index.js
@@ -12,10 +12,9 @@ const MERKLE_ROOT = '';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { proof, name, root} = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, root);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
